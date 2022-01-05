180d8a68ff5f5409f2dfe12ac6a203c1a1f3d07b
fauzan07 <fauzanfalke@gmail.com>
2022-01-05 12:53:10 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button, Modal } from 'react-bootstrap';
+import { useState } from 'react';
 
 function App() {
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +18,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Button variant="primary">Primary</Button>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,9 +27,32 @@ function App() {
         >
           Learn React
         </a>
+        <Button variant="primary" onClick={handleShow}>
+        Launch demo modal
+      </Button>
       </header>
+
+      
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
     </div>
   );
+
+  
 }
 
 export default App;
