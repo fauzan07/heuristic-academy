96b4bf0268fc2e0c338e6642e728e091a2e86eb6
fauzan07 <fauzanfalke@gmail.com>
2022-01-12 11:34:08 +0530

    updated new web

--- src/App.js
+++ src/App.js
@@ -1,58 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import Header from "./components/Header/Header";
+import Main from "./pages/Main/Main";
+// import Footer from "./components/Footer/Footer";
+import ScrollToTop from "./ScrollToTop"
 import './App.css';
-import { Button, Modal } from 'react-bootstrap';
-import { useState } from 'react';
+
 
 function App() {
-
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Button variant="primary">Primary</Button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button variant="primary" onClick={handleShow}>
-        Launch demo modal
-      </Button>
-      </header>
-
-      
-
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
-
-    </div>
-  );
-
   
+  return (
+  <>
+       <BrowserRouter>
+       <ScrollToTop>
+        <Routes>
+          <Route exact path="/" element={<Main />} />
+        </Routes>
+        {/* <Header />
+        <Switch>
+          <Route exact path="/"  component={Home} />
+        </Switch>
+        <Footer /> */}
+        </ScrollToTop>
+      </BrowserRouter>
+</>
+  );
 }
 
 export default App;
